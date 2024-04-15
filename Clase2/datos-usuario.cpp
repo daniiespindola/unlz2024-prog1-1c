4a1bf86edc784952d8534d483613a0eb7b78a3c6
daniiespindola <riverplate.dani33@gmail.com>
2024-04-14 22:25:41 -0300

    se agrega getline para poder tomar toda la cadena de datos ingresada

--- Clase2/datos-usuario.cpp
+++ Clase2/datos-usuario.cpp
@@ -1,53 +1,53 @@
-#include <iostream> 
+#include <iostream>
 #include <string>
-#include<cmath>
 
 using namespace std;
 
-int main() { 
-    //declaro variables
-     string nombre,apellido, direccion, localidad, provincia, pais, telefono;
-     int edad, altura;
-       
-    //solicito dqatos al usuario   
-    cout <<"Ingrese su nombre: "<<endl;
-    cin >> nombre;
-    
-    cout <<"Ingrese su apellido: "<<endl;
-    cin >> apellido;
+int main() {
+    // Declarar variables
+    string nombre, apellido, direccion, localidad, provincia, pais, telefono;
+    int edad, altura;
 
-    cout <<"Ingrese su edad: "<<endl;
+    // Solicitar datos al usuario
+    cout << "Ingrese su nombre completo: " << endl;
+    getline(cin, nombre);
+
+    cout << "Ingrese su apellido: " << endl;
+    getline(cin, apellido);
+
+    cout << "Ingrese su edad: " << endl;
     cin >> edad;
-    
-    cout <<"Ingrese calle donde vive y luego la altura: "<<endl;
-    cin >> direccion;
+
+    cout << "Ingrese la calle donde vive y luego la altura: " << endl;
+    cin.ignore();
+    getline(cin, direccion);
     cin >> altura;
-    
-    cout <<"Ingrese su localidad: "<<endl;
-    cin >> localidad;
-    
-    cout <<"Ingrese su provincia: "<<endl;
-    cin >> provincia;
-    
-    cout <<"Ingrese su pais de origen: "<<endl;
-    cin >> pais;
-    
-    cout <<"Ingrese su telefono: "<<endl;
+
+    cout << "Ingrese su localidad: " << endl;
+    cin.ignore();
+    getline(cin, localidad);
+
+    cout << "Ingrese su provincia: " << endl;
+    cin.ignore(); 
+    getline(cin, provincia);
+
+    cout << "Ingrese su país de origen: " << endl;
+    cin.ignore(); 
+    getline(cin, pais);
+
+    cout << "Ingrese su teléfono: " << endl;
     cin >> telefono;
 
-
-    //muestro resultados ingresados
-    cout<<"Usted completo"<<endl;
-    cout<<"Nombre: "<<nombre<<endl;
-    cout<<"Apellido: "<<apellido<<endl;
-    cout<<"Edad: "<<edad<<endl;
-    cout<<"Direccion: "<<direccion<< " " <<altura<<endl;
-    cout<<"Localidad: "<<localidad<<endl;
-    cout<<"Provincia: "<<provincia<<endl;
-    cout<<"Pais: "<<pais<<endl;
-    cout<<"Telefono: "<<telefono<<endl;
+    // Mostrar los resultados ingresados
+    cout << "Usted completó" << endl;
+    cout << "Nombre: " << nombre << endl;
+    cout << "Apellido: " << apellido << endl;
+    cout << "Edad: " << edad << endl;
+    cout << "Dirección: " << direccion << " " << altura << endl;
+    cout << "Localidad: " << localidad << endl;
+    cout << "Provincia: " << provincia << endl;
+    cout << "País: " << pais << endl;
+    cout << "Teléfono: " << telefono << endl;
 
     return 0;
-    	
-		
-} 
+}
