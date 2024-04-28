878a772ffd059eb57879a9438c3ed1c0817c6b52
daniiespindola <riverplate.dani33@gmail.com>
2024-04-28 20:17:47 -0300

    se hacen ejercicio 4 y 5

--- Clase5/ejercicio4.cpp
+++ Clase5/ejercicio4.cpp
@@ -1,4 +1,3 @@
-//
 #include <iostream>
 #include <string>
 
@@ -7,5 +6,19 @@ using namespace std;
 //Implementar un juego de adivinanzas en el que el usuario tenga que adivinar un número secreto. 
 //El ciclo while se repetirá hasta que el usuario adivine el número correcto
 int main() { 
+//declaro variables
+int num = 0;
+int numSecreto = 33;
+
+while (num != numSecreto)
+{
+    cout << "ADIVINE EL NUMERO SECRETO, ES UN NUMERO ENTERO ENTRE EL 1 Y EL 100" << endl;
+    cin >> num; 
+    //cout << "intente nuevamente" << endl;
+}
+
+cout << "Ha ganado el numero secreto era " << num << endl;
+
+return 0;
 
 }
--- Clase5/ejercicio5.cpp
+++ Clase5/ejercicio5.cpp
@@ -7,5 +7,21 @@ using namespace std;
 //Desarrollar un programa que pida al usuario ingresar una contraseña, 
 //y se repita hasta que ingrese la contraseña correcta.
 int main() { 
+    string contraseñaCorrecta = "123456";
+    string contraseñaIngresada;
 
+    // do while para que al menos una vez ingrese una clave
+    do {
+        cout << "Ingrese la contraseña: ";
+        cin >> contraseñaIngresada;
+
+        if (contraseñaIngresada != contraseñaCorrecta) {
+            cout << "Contraseña incorrecta. Por favor, intente de nuevo." << endl;
+        }
+
+    } while (contraseñaIngresada != contraseñaCorrecta);
+
+    cout << "Contraseña correcta" << endl;
+
+    return 0;
 }
