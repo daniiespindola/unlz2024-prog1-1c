ca9c1eb32373f37d3d35048d1e38a8e3cc644d0d
daniiespindola <riverplate.dani33@gmail.com>
2024-04-29 20:27:07 -0300

    se agregan comentarios

--- Clase6/ejercicio1.cpp
+++ Clase6/ejercicio1.cpp
@@ -8,17 +8,19 @@
 using namespace std; 
 
 int main() {
+	//declaro variables
 	int numIngresado = 0;
 	int multiplicador = 0;
 	int multiplicacion = 0;
 
+	//uso ciclo while para verificar que el num ingresado sea del 1 al 9
 	while(numIngresado < 1 || numIngresado > 10){
 		cout << "Recuerde que el numero debe ser del 1 al 9 " << endl;
 		cout << "Ingrese un numero entero del 1 al 9: " << endl;
 		cin >> numIngresado;		
 		}
 
-
+	//ciclo while usando multiplicador el cual sumara de a 1 hasta llegar a 10 y se multiplicara al num ingresado
 	while(multiplicador <= 10){
 			multiplicacion = numIngresado * multiplicador;
 			cout << "el num ingresado fue " << numIngresado << " X " << multiplicador << " = " << multiplicacion << endl;
--- Clase6/ejercicio2.cpp
+++ Clase6/ejercicio2.cpp
@@ -1,10 +1,11 @@
-//contador de pares y contador de impares, 
-//esperar por pantalla n numeros
-//contar cantidad de pares ingresados
-//contar cantidad de impares ingresados 
-//cortar ciclo cuando se ingrese 0
-//mostrar valor de contador por pantalla
-
+/*
+contador de pares y contador de impares, 
+esperar por pantalla n numeros
+contar cantidad de pares ingresados
+contar cantidad de impares ingresados 
+cortar ciclo cuando se ingrese 0
+mostrar valor de contador por pantalla
+*/
 
 #include <iostream>
 #include <string>
@@ -12,15 +13,17 @@
 using namespace std; 
 
 int main() {
+	//declaro variables
 	int num = 0;
 	int contadorPar = 0;
 	int contadorImpar = 0;
 	
-	
+	//uso ciclo do while 
 	do{
 		cout << "ingrese un numero, si ingresa 0 se termina el programa: " << endl;
 		cin >> num;
 		
+		//if para verificar si es par o impar. validacion distinta a cero para que no lo tome como par
 		if (num !=0){
 		if (num%2==0){
 			contadorPar++;
@@ -33,8 +36,9 @@ int main() {
 		}
 		}	
 		
-	}while (num != 0);
+	}while (num != 0);//mientras que el num ingresado sea distinto a cero ejecutara el ciclo
 	
+	//muestro resultados por pantalla
 	cout << "impares ingresados " << contadorImpar << " y pares ingresados " << contadorPar  << endl;
 	cout << "Ingreso 0 finalizo el programa" << endl;
 	
